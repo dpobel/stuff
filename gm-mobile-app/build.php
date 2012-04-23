5d60ab9eee57c897eadb24630686209d65149c08
Damien Pobel <dp@ez.no>
2012-04-23 08:00:59 +0200

    Added a JSON export of the stations list during the build

--- gm-mobile-app/build.php
+++ gm-mobile-app/build.php
@@ -8,7 +8,24 @@ define( 'WWW_ROOT', $ini['app']['root'] );
 define( 'PUBLIC_CACHE_DIR', $ini['app']['public_cache_dir'] );
 
 $vars = array( 'settings' => $ini );
+
+echo "Generating {$ini['app']['index_template']}\n";
 $content = template( $ini['app']['index_template'], $vars );
+
+echo "Building the station list\n";
+$lines = file( $ini['app']['data_file'] );
+array_shift( $lines );
+$result = array();
+foreach ( $lines as $l )
+{
+    list( $name, $x, $y, $lat, $lon ) = explode( ';', trim( $l ) );
+    $result[] = array(
+        'name' => trim( $name, '"' ),
+        'lat' => (float) str_replace( ',', '.', $lat ),
+        'lon' => (float) str_replace( ',', '.', $lon )
+    );
+}
+file_put_contents( $ini['app']['json_data_file'], json_encode( $result ) );
 
 if ( $content !== null )
 {
