171869b82208736aa0e7b46bd9622385565e02e5
Damien Pobel <damien@pobel.fr>
2012-12-23 20:11:45 +0100

    Added mogrify to automagically fix the orientation

--- album/generate.sh
+++ album/generate.sh
@@ -5,6 +5,8 @@ RESIZE="8%"
 
 for f in `find . -name \*.JPG` ; do
     N=`echo $f | sed s/JPG/jpg/g`
+    echo "Auto fixing the orientation"
+    mogrify -auto-orient $f
     echo "$f => $N"
     convert -resize $RESIZE $f $N
 done
